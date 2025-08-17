a8b2f2347a6020f68dc73519c756236a1ca50834
gaurav <gauravbordia2@gmail.com>
2025-08-17 17:27:02 +0530

    added datatypes file and updated variables file

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -4,8 +4,11 @@ var accountPass = "12345"
 accountCity = "Jaipur"
 
 //accountId  = 2 
-
+/* 
+prefer not to use var
+because of issue in block scope and functional scope 
+*/
 accountEmail = "hchg@2"
 accountPass = "212121"
 
-console.table[(accountEmail , accountPass)]k
+console.table([accountEmail , accountPass])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,15 @@
+"use strict"; //treat all js code as newer version
+
+alert( 3 + 3)
+
+
+ /*
+ number
+BigInt
+string = "0"
+boolean
+null
+undefined
+*/
+
+// OBJECT
