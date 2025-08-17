0c521976883534be5ede55bc49b5aa4effbbd5f4
gaurav <gauravbordia2@gmail.com>
2025-08-17 16:52:22 +0530

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPass = "12345"
+accountCity = "Jaipur"
+
+//accountId  = 2 
+
+accountEmail = "hchg@2"
+accountPass = "212121"
+
+console.table[(accountEmail , accountPass)]k
