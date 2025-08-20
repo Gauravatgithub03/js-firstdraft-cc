282f5f9d85b3012fcc0592cc1d4a0e2667c019b7
gaurav <gauravbordia2@gmail.com>
2025-08-20 06:51:13 +0530

    first commit

---
+++ 01_basics/04_datatypes-summary.js
@@ -0,0 +1,27 @@
+
+console.log("✅ This file is running...");
+
+const heroes = ["ironaman" , "spiderman"  , "hulk"]
+let myObj = {
+    name : "gaurav",
+    age: 22,
+
+}
+
+const myFunction = function(){
+    console.log("hello world");
+}
+
+console.log(typeof myObj)
+
+/* # Primitive datatypes
+
+7 types : string , Number , Boolean , null ,  Undefined , Symbol , BigInt 
+
+
+
+ # Refrence type / Non primitive datatypes
+
+Array , Objects , Functions 
+*/
+
